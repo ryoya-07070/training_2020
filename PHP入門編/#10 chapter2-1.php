9e6d49bcb0c103f7cf8b3a9531ad2bbe7c01b0d7
Ryoya Nakano <ryosun7@gmail.com>
2020-04-09 10:48:13 +0900

    PHP入門編１０: 例外処理

---
+++ PHP入門編/#10 chapter2-1.php
@@ -0,0 +1,14 @@
+<?php
+// 例外処理を定義しよう
+
+echo "start\n";
+
+try {$date = new DateTime("199x-01-01");
+       echo $date->format('Y/m/d') . "\n";
+    } catch (Exception $e ) {
+        echo $e->getMessage()."\n";
+    } finally {
+        echo "end\n";
+}
+
+?>
